56ad1747c08d9966d2f030976a62d7a2540da4d2
Shaina C <scailles23@gmail.com>
2020-01-30 10:30:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,6 +26,8 @@ class Scraper
       course= Course.new
       course.title = post.css("h2").text
       
+      course.schedule = post.css("date").text
+      
       course.description = post.css("p").text
     end
   end
